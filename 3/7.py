e93056df0e0bf185349e6f8ec60f7d568f5962d4
Junhui Dong <2365354490@qq.com>
2017-05-20 13:45:55 +0800

    Fix 3-7: use \' instead of '

--- 3/7.py
+++ 3/7.py
@@ -1,4 +1,4 @@
-fe = float(input('Enter your car's fuel economy (L/100km): '))
+fe = float(input('Enter your car\'s fuel economy (L/100km): '))
 fe = 1.0 / fe * 100.0	# km/L
 fe = fe * 0.6214	# mile/L
 fe = fe * 3.875		# mile/gallon
