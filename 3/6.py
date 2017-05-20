878c1583668f9b9ef9b71737127edfef57c8af75
Junhui Dong <2365354490@qq.com>
2017-05-20 13:36:27 +0800

    3-6

---
+++ 3/6.py
@@ -0,0 +1,3 @@
+distance = float(input('Enter the distance driven, in miles: '))
+gas = float(input('Enter the number of the gas used, in gallons: '))
+print('Gas mileage:', distance / gas, 'miles per gallon.')
