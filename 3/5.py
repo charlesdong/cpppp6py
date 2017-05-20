14ff77fa91c12fb6148b4342a39ea3450c6a1c9d
Junhui Dong <2365354490@qq.com>
2017-05-20 13:32:57 +0800

    3-5

---
+++ 3/5.py
@@ -0,0 +1,3 @@
+wp = int(input('Enter the world's population: '))
+up = int(input('Enter the population of the US: '))
+print('The population of the US is ', up/wp*100.0, '% of the world population.', sep='')
