951b0e39215a567ad16c6f7cbdd0c8e31e863a7f
Junhui Dong <2365354490@qq.com>
2017-05-20 16:34:39 +0800

    4-6

---
+++ 4/6.py
@@ -0,0 +1,12 @@
+class CandyBar:
+    def __init__(self, name, weight, heat):
+        self.name = name
+        self.weight = weight
+        self.heat = heat
+
+L = [CandyBar('Mocha Munch', 2.3, 350), CandyBar('Sweet Candy Sweet', 3.0, 600), CandyBar('Yummie', 2.5, 320)]
+for i, cb in enumerate(L):
+    print('Candy bar %d:' % (i + 1))
+    print('Name:', cb.name)
+    print('Weight:', cb.weight)
+    print('Heat quantity:', cb.heat)
