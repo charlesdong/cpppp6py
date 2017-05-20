518d8c498491864920839f9d1eac2ffbb952b6ab
Julio Oliveira <antoniojuliodeo@gmail.com>
2017-05-20 03:00:12 -0300

    Update 5.py
    that's better ^^ .

--- 2/5.py
+++ 2/5.py
@@ -2,4 +2,4 @@ def c2f(cel):
   return cel * 1.8 + 32.0
 
 cel = float(input('Please enter a Celsius value: '))
-print(cel, 'degrees Celsius is', c2f(cel), 'degrees Fahrenheit.')
+print('%.1f degrees Celsius is %.1f degrees Fahrenheit.'%(cel, c2f(cel)))
